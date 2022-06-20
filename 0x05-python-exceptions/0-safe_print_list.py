60616732b7beb12a585de692000561dae9693232
Oluebube5 <100016422+Oluebube5@users.noreply.github.com>
2022-06-20 22:16:42 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except:
-        return False
+
+
+def safe_print_list(my_list=[], x=0):
+    elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        except:
+            pass
+    print()
+    return elements
